a1e804880e90fd2aa5d6107ff9362e6c83135ea5
Debin <luodebin@kylinos.cn>
2025-11-25 15:32:55 +0800

    更新依赖项并调整 NMI 模块结构

--- src/nmi.rs
+++ src/nmi.rs
@@ -1,11 +1,11 @@
-use axplat_aarch64_peripherals::{pmu,gic};
+use axplat::irq;
 use log::warn;
 
-pub fn init(irq_num:usize) {
-    warn!("nmi init");
-    gic::set_priority(irq_num as u32,0);
-    pmu::init(0xf0000000);
-}
+// pub fn init(irq_num:usize) {
+//     warn!("nmi init");
+//     gic::set_priority(irq_num as u32,0);
+//     pmu::init(0xf0000000);
+// }
 
 pub fn handle(){
     warn!("nmi handle");
